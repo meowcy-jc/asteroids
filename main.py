2da3947af7f0cde2ee3fc1ccd0d3b1476e664f24
meowcy-jc <joyce.chen.personal@gmail.com>
2025-11-05 16:11:33 -0500

    set up 60FPS

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ def main():
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0
+
     running = True
     while running:
         for event in pygame.event.get():
@@ -18,6 +21,9 @@ def main():
 
         screen.fill((0, 0, 0))
         pygame.display.flip()
+
+        clock.tick(60)
+        dt = clock.tick(60) /1000
 
 
 if __name__ == "__main__":
