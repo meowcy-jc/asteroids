0cd939837a4adb02cdb0539a73120e78a28562a7
meowcy-jc <joyce.chen.personal@gmail.com>
2025-11-06 23:19:03 -0500

    made asteriods move and added shots

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
+import sys
+
 import pygame
-from logger import log_state
-from constants import *
-from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
+from constants import *
+from logger import log_event, log_state
+from player import Player
+
 
 def main():
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
@@ -17,17 +17,18 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
+
+    Shot.containers = (shots, updatable, drawable)
 
     Player.containers = (updatable, drawable)
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
-    Asteroid.containers = (updatable, drawable, asteroids)
-
-    AsteroidField.containers = (updatable)
-
-    AsteroidField()
-    
     dt = 0
 
     while True:
@@ -39,16 +40,22 @@ def main():
 
         updatable.update(dt)
 
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
+
         screen.fill("black")
 
-        for player in drawable:
-            player.draw(screen)
+        for obj in drawable:
+            obj.draw(screen)
 
         pygame.display.flip()
 
-        dt = clock.tick(60) /1000
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
     main()
-
--- shot.py
+++ shot.py
@@ -1,8 +1,9 @@
 import pygame
 from circleshape import CircleShape
+from constants import *
 
 
-class Asteroid(CircleShape):
+class Shot(CircleShape):
     def __init__(self, x, y, radius):
         super().__init__(x, y, radius)
 
